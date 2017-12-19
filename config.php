3a510bed691141073a7f8edb68498afd31d2754d
Monsta <MonstaApps@users.noreply.github.com>
2017-12-20 12:53:02 +1300

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$mysql_host = ""; // MySQL host address
+$mysql_user = ""; // MySQL username
+$mysql_pass = ""; // MySQL password
+$mysql_db   = ""; // MySQL database name
+
+$imap_host  = ""; // IMAP host address
+$imap_flags = "/pop3/novalidate-cert"; // IMAP Flags
+$imap_user  = ""; // IMAP username
+$imap_pass  = ""; // IMAP password
+
+$file_store = "files"; // Folder where file attachments are saved
+$grab_type  = "fetch"; // Type of mail grab - "pipe" or "fetch"
